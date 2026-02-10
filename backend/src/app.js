bc65fed64a0d69288a62eea75d7f2074e2c8974b
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2026-02-10 20:59:03 +0500

    Fix issue with cors via dotenv

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import songsRouter from "./routes/songs.js";
 import cors from "cors";
+import "dotenv";
 
 const app = express();
 
 app.use(express.json());
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: process.env.FRONTEND_URL,
 	}),
 );
 app.use("/api/songs", songsRouter);
